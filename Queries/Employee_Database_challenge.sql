bce8d203de2e619890eeda48738798b224fcf7a2
annarochav <101613210+annarochav@users.noreply.github.com>
2022-05-19 22:07:45 -0700

    challenge update

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -24,7 +24,7 @@ rt.first_name,
 rt.last_name,
 rt.title
 INTO unique_titles
-FROM retirement_titles AS rt
+FROM retirement_titles as rt
 WHERE (rt.to_date = '9999-01-01')
 ORDER BY rt.emp_no, rt.to_date DESC;
 
@@ -40,6 +40,38 @@ ORDER BY count DESC;
 
 --Preview output
 SELECT * FROM retiring_titles
+
+----------------------------------------------------------------------------------------------
+--DELIVERABLE 2 
+----------------------------------------------------------------------------------------------
+--Mentorship Employees Eligibility Program
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name, 
+	e.last_name, 
+	e.birth_date,
+	d.from_date,
+	d.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as d
+ON (e.emp_no = d.emp_no)
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (d.to_date = '9999-01-01')
+ORDER BY e.emp_no, ti.from_date DESC;
+--Preview output
+SELECT * FROM mentorship_eligibilty
+
+
+
+
+
+
+
+
+
 
 
 
